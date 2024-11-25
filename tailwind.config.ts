a8f306ab919f52149bd91b40fb363bc0356527fc
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-11-25 16:14:10 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,54 +5,54 @@ export default {
   darkMode: ["class"],
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  	extend: {
-  		fontFamily: {},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		screens: {
-  			pocket: '300px',
-  			tablet: '750px',
-  			desktop: '1285px'
-  		},
-  		colors: {
-  			primaryBg: '#F5F5F5',
-  			secondaryBg: '#ECECEC',
-  			primaryBtn: ' #28A745',
-  			primaryBtnHover: '#218838',
-  			secondaryBtn: '#007BFF',
-  			secondaryBtnHover: '#0056b3',
-  			warning: '#FFC107',
-  			warningHover: '#FF8800',
-  			info: '#17A2B8',
-  			infoHover: '#0096C7',
-  			danger: '#DC3545',
-  			dangerHover: '#C82333',
-  			formBg: '#F8F9FA',
-  			formHeaderBg: '#121063',
-  			inputBorder: '#CED4DA',
-  			inputBorderFocus: '#80BDFF',
-  			inputText: '#495057',
-  			placeHolderText: '#6C757D',
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		},
-  		boxShadow: {
-  			sm: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-  			md: '0px 4px 8px rgba(0, 0, 0, 0.15)',
-  			lg: '0px 6px 12px rgba(0, 0, 0, 0.2)'
-  		}
-  	}
+    extend: {
+      fontFamily: {},
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      screens: {
+        pocket: "300px",
+        tablet: "750px",
+        desktop: "1285px",
+      },
+      colors: {
+        primaryBg: "#F5F5F5",
+        secondaryBg: "#ECECEC",
+        primaryBtn: " #28A745",
+        primaryBtnHover: "#218838",
+        secondaryBtn: "#007BFF",
+        secondaryBtnHover: "#0056b3",
+        warning: "#FFC107",
+        warningHover: "#FF8800",
+        info: "#17A2B8",
+        infoHover: "#0096C7",
+        danger: "#DC3545",
+        dangerHover: "#C82333",
+        formBg: "#F8F9FA",
+        formHeaderBg: "#121063",
+        inputBorder: "#CED4DA",
+        inputBorderFocus: "#80BDFF",
+        inputText: "#495057",
+        placeHolderText: "#6C757D",
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
+      },
+      boxShadow: {
+        sm: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+        md: "0px 4px 8px rgba(0, 0, 0, 0.15)",
+        lg: "0px 6px 12px rgba(0, 0, 0, 0.2)",
+      },
+    },
   },
   plugins: [
     require("tailwindcss-motion"),
