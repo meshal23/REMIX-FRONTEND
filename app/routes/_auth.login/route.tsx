30ea7a6afeaebbc4559e4e87f580237cb29d07eb
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-12-09 16:22:59 +0530

    login form ui created

--- app/routes/_auth.login/route.tsx
+++ app/routes/_auth.login/route.tsx
@@ -1,5 +1,66 @@
+import { Form, Link } from "@remix-run/react";
+
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <section className="w-full min-h-screen flex flex-col justify-center items-center">
+      <div className="text-center w-full p-4 mb-5 font-roboto-bold text-3xl">
+        LOGIN HERE
+      </div>
+      <Form className="w-11/12 ">
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            style={{ width: "100%" }}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@flowbite.com"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          style={{ width: "100%" }}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Login
+        </button>
+      </Form>
+      <p
+        id="helper-text-explanation"
+        className="mt-2 text-sm text-gray-500 dark:text-gray-300"
+      >
+        Create a new account
+        <Link
+          to="/register"
+          className="font-medium text-blue-600 hover:underline ml-2 dark:text-blue-500"
+        >
+          Register Here
+        </Link>
+        .
+      </p>
+    </section>
+  );
 };
 
 export default Login;
